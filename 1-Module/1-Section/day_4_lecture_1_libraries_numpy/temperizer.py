777a9cea63408713ad41546effcc2e689ed88c98
aapeebles <alison.peeblesmadigan@flatironschool.com>
2019-07-17 19:10:44 -0400

    add day4 lecture

---
+++ 1-Module/1-Section/day_4_lecture_1_libraries_numpy/temperizer.py
@@ -0,0 +1,12 @@
+"""An example library for converting temperatures."""
+
+
+def convert_f_to_c(temperature_f):
+    """Convert Fahrenheit to Celsius."""
+    temperature_c = (temperature_f - 32) * (5/9)
+    return temperature_c
+
+
+def convert_c_to_f(temperature_c):
+    """Convert Celsius to Fahrenheit."""
+    pass
