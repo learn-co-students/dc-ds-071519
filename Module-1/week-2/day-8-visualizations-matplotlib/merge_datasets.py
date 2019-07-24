1182dc976fa4347eb4907de6caba1f0ea856fef4
aapeebles <alison.peeblesmadigan@flatironschool.com>
2019-07-24 17:20:05 -0400

    add solutions for today's lectures

--- Module-1/week-2/day-8-visualizations-matplotlib/merge_datasets.py
+++ Module-1/week-2/day-8-visualizations-matplotlib/merge_datasets.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-def merge_intake_outcomes():
-        """
+def merge_intake_outcomes(data1, data2):
+    """
     This function:
         - merges intake_dataset with outcomes_dataset
         - creates new variable `days_in_shelter`
         - returns animal_shelter_df
     """
-    animal_shelter_df  = pd.merge(intake_dataset, outcomes_dataset, on=['id', 'year'],                                   how='left', suffixes=('_intake', '_outcome'))
+    animal_shelter_df  = pd.merge(data1, data2, on=['id', 'year'], how='left', suffixes=('_intake', '_outcome'))
 
     animal_shelter_df = animal_shelter_df[(~animal_shelter_df['o_date'].isna()) &                                           (animal_shelter_df['o_date'] > animal_shelter_df['i_date'])]
 
