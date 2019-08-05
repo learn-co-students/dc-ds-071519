d428ac18ad456df3bdde67de60cceff8df790a7e
aapeebles <alison.peeblesmadigan@flatironschool.com>
2019-08-05 12:26:31 -0400

    add json api lecture

---
+++ Module-2/week-2/day_1_json_xml_apis/yelp_keys.py
@@ -0,0 +1,11 @@
+"""stores the yelp api client id and api key in a separate file for security and best practice"""
+
+def return_api_key():
+"""returns api key for yelp api"""
+    api_key = ''"
+    return api_key
+
+def return_client_id():
+"""returns client id for yelp api"""
+    client_id = ''
+    return client_id
