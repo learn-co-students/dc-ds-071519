b9c2931cff14aed2f91767be8b7fe466ea196059
Marisa Mitchell <marisa.mitchell@flatironschool.com>
2019-08-14 08:43:06 -0400

    adding warm up

---
+++ Warm_Ups/Dictionary_Practice_1.py
@@ -0,0 +1,5 @@
+# Dictionary Practice #1
+
+#Given the below dictionary called dict, print each of the keys, each on their own line.
+
+dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog', 'pizza':'veggie', 'movie':'titanic', 'drink':'coffee'}
