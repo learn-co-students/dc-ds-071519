f37bc58a54232db9ac800dfd2a02413aacba3b16
Marisa Mitchell <marisa.mitchell@flatironschool.com>
2019-08-15 13:54:36 -0400

    adding warm up solutions

---
+++ Warm_Ups/Dictionary_Practice_1-Solution.py
@@ -0,0 +1,8 @@
+# Dictionary Practice #1
+
+#Given the below dictionary called dict, print each of the keys, each on their own line.
+
+dict = {'color': 'blue', 'fruit': 'apple', 'pet': 'dog', 'pizza':'veggie', 'movie':'titanic', 'drink':'coffee'}
+
+for key in dict.keys():
+    print(key)
