f1f83995838175881b283bba486368b9d7041256
Marisa Mitchell <marisa.mitchell@flatironschool.com>
2019-08-27 10:18:23 -0400

    adding multiple regression warm up

---
+++ Warm_Ups/Multiple_regression_1.py
@@ -0,0 +1,6 @@
+# Multiple Regression Practice #1
+
+
+# Conduct a linear regression model using the bottle dataset (https://www.kaggle.com/sohier/calcofi) to answer the following question: Is there a linear relationship between water salinity & water temperature? 
+
+#Be sure to report on the correlation and covariance between these variables as well as the summary statistics from the regression model
