8c077af4910ad65d5421be8232edbd5100e9a418
mmitchell-FIS <52426474+mmitchell-FIS@users.noreply.github.com>
2019-08-27 10:25:22 -0400

    Update Linear_regression_1.py

--- Warm_Ups/Linear_regression_1.py
+++ Warm_Ups/Linear_regression_1.py
@@ -1,4 +1,4 @@
-# Multiple Regression Practice #1
+# Linear Regression Practice #1
 
 
 # Conduct a linear regression model using the bottle dataset (https://www.kaggle.com/sohier/calcofi) to answer the following question: Is there a linear relationship between water salinity & water temperature? 
